e4e863410b4ccd2c627002a7f1aaec1f1b82301b
ajharul64 <ajharul.islam@barclays.com>
2024-05-15 00:02:52 +0100

    tests added

--- models/Musician.js
+++ models/Musician.js
@@ -1,8 +1,14 @@
 const {Sequelize, sequelize} = require('../db');
+const { DataTypes} = require('sequelize');
 
 // TODO - define the Musician model
 let Musician;
+Musician = sequelize.define('Musician',{
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING
 
+}
+);
 module.exports = {
     Musician
 };
--- models/Song.js
+++ models/Song.js
@@ -1,8 +1,16 @@
+const { DataTypes } = require('sequelize');
 const {Sequelize, sequelize} = require('../db');
+
 
 // TODO - define the Song model
 let Song;
+Song= sequelize.define('Song',{
+    title: DataTypes.STRING,
+    year: DataTypes.STRING,
+    length: DataTypes.STRING
 
+}
+);
 module.exports = {
     Song
 };
